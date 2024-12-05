307e44e08b4ce7702eba566b44a2be1d3399e31f
KarthikSapaliga <im.karthiksapaliga@gmail.com>
2024-12-05 23:47:43 +0530

    Add Metadata section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,42 +1,58 @@
 import { BiSearch } from "react-icons/bi";
-import './Main.css'
+import "./Main.css";
+
+import Metadata from "./Metadata";
+
 
 const Main = ({ handleSectionChange, activeSection }) => {
     return (
-        <div className='container'>
-            { activeSection==='home' && (
+        <div className="container">
+            {activeSection === "home" && (
                 <div id="home">
-                    <div className="search-box" onClick={()=>handleSectionChange('search')}>
+                    <div
+                        className="search-box"
+                        onClick={() => handleSectionChange("search")}
+                    >
                         <BiSearch />
                         <span>Search here...</span>
                     </div>
-                    <div className="all-images">All Images</div>
-                    <div className="categorize" onClick={()=>handleSectionChange('categorizer')} >Categorize</div>
-                    <div className="metadat-stripp" onClick={()=>handleSectionChange('metadata-stripper')} >Metadata</div>
+                    <div className="feature-container">
+                        <div className="all-images">All Images</div>
+                        <div
+                            className="categorizer"
+                            onClick={() => handleSectionChange("categorizer")}
+                        >
+                            Categorize
+                        </div>
+                        <div
+                            className="metadat-stripper"
+                            onClick={() => handleSectionChange("metadata-stripper")}
+                        >
+                            Metadata
+                        </div>
+                    </div>
                 </div>
-            ) }
-            { activeSection==='search' && (
+            )}
+            {activeSection === "search" && (
                 <div id="search">
                     <h1>Search</h1>
                 </div>
-            ) }
-            { activeSection==='categorizer' && (
+            )}
+            {activeSection === "categorizer" && (
                 <div id="categorizer">
                     <h1>Categorizer</h1>
                 </div>
-            ) }
-            { activeSection==='metadata-stripper' && (
-                <div id="metadata-stripper">
-                    <h1>Metadata Stripper</h1>
-                </div>
-            ) }
-            { activeSection==='settings' && (
+            )}
+            {activeSection === "metadata-stripper" && (
+                <Metadata />
+            )}
+            {activeSection === "settings" && (
                 <div>
                     <h1>Settings</h1>
                 </div>
-            ) }
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default Main
+export default Main;
