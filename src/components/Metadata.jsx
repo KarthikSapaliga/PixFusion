e4bd5e6f46e19c25f0cb51e04998cd4d4cfe0d76
KarthikSapaliga <im.karthiksapaliga@gmail.com>
2024-12-07 19:32:53 +0530

    Modified the layout

--- src/components/Metadata.jsx
+++ src/components/Metadata.jsx
@@ -3,7 +3,9 @@ import { useState } from 'react'
 import axios from 'axios'
 import './Metadata.css'
 
-import { FaCloudUploadAlt } from "react-icons/fa";
+import { BiSolidCloudUpload } from "react-icons/bi";
+import ReactLoading from 'react-loading';
+import { MdMargin } from 'react-icons/md';
 
 const Metadata = () => {
 
@@ -13,11 +15,15 @@ const Metadata = () => {
     const [cleanedFileUrl, setCleanedFileUrl] = useState(null);
     const [downloadUrl, setDownloadUrl] = useState(null);
 
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
     const handleFileChange = (e) => {
         setSelectedFile(e.target.files[0]);
     };
 
     const handleUpload = async () => {
+        setLoading(true)
         if (!selectedFile) {
             alert("Please select a file first.");
             return;
@@ -40,43 +46,56 @@ const Metadata = () => {
             const filename = cleaned_file_path.split("/").pop();
             setCleanedFileUrl(filename);
             setDownloadUrl(filename);
-        } catch (error) {
-            console.error("Error uploading file:", error);
-            alert("Failed to upload file.");
+        } catch (err) {
+            setError("Error fetching data: " + err.message);
+        } finally {
+            setLoading(false);
         }
     };
 
     return (
         <div id="metadata-stripper">
             <div className="image-select">
-                <div className="select">
-                    <input type="file" onChange={handleFileChange} id='fileInput' />
-                    <label htmlFor="fileInput">
-                        <div>
-                            <FaCloudUploadAlt size={300} />
-                            <span>Upload the Image</span>
-                        </div>
-                    </label>
-                    { selectedFile && (
-                        <button onClick={handleUpload}>Process Metadata</button>
-                    ) }
-                </div>
+                <input type="file" onChange={handleFileChange} id='fileInput' />
+                <label htmlFor="fileInput">
+                    <BiSolidCloudUpload size={120} />
+                    <h1>Upload the Image</h1>
+                </label>
+
             </div>
-            <div className="view-metadata">
-                <div className="prev-metadata">
-                    <h1>Original Metadata</h1>
-                    <pre>{originalMetadata}</pre>
+            {selectedFile && (
+                <button onClick={handleUpload}>Process Metadata</button>
+            )}
+
+            {loading && (
+                <div className='loading'>
+                    <ReactLoading type='bars' color={"#aaa"} width={80} />
                 </div>
-                <div className="cleaned-metadata">
-                    <h1>
-                        <span>Cleaned Metadata</span>
-                        {cleanedFileUrl && (
-                            <a href={downloadUrl} download="cleaned">Download</a>
-                        )}
-                    </h1>
-                    <pre>{metadata}</pre>
+            )}
+            
+            {error && <div>{error}</div>}
+
+            { metadata && (
+                <div className="metadata-container">
+                    <div className="original-metadata">
+                        <h1>Original Metadata</h1>
+                        <pre>
+                            {originalMetadata}
+                        </pre>
+                    </div>
+                    <div className="cleaned-metadata">
+                        <h1>
+                            <>Cleaned Metadata</>
+                            {cleanedFileUrl && (
+                                <a href={downloadUrl} download="cleaned">Download Cleaned Image</a>
+                            )}
+                        </h1>
+                        <pre>
+                            {metadata}
+                        </pre>
+                    </div>
                 </div>
-            </div>
+            ) }
         </div>
     )
 }
