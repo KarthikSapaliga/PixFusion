252e68a90f13c37b5ce451725d3ce6c859f6c294
KarthikSapaliga <im.karthiksapaliga@gmail.com>
2024-12-18 14:44:54 +0530

    Added buttons to navigate

--- src/components/ImageClassifier.jsx
+++ src/components/ImageClassifier.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState,useEffect } from 'react';
+import { useState,useEffect,useRef } from 'react';
 import axios from 'axios'
 import ReactLoading from 'react-loading';
 
@@ -9,6 +9,7 @@ const ImageClassifier = () => {
     const [category, setCategory] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
+    const categoryRefs = useRef({})
 
     useEffect(() => {
         axios
@@ -24,6 +25,12 @@ const ImageClassifier = () => {
             });
     }, []);
 
+    const scrollToCategory = (categoryName) => {
+        if (categoryRefs.current[categoryName]) {
+            categoryRefs.current[categoryName].scrollIntoView({ behavior: "smooth" });
+        }
+    }; 
+
     if (loading) return <div>
         <ReactLoading type='spin'/>
     </div>;
@@ -32,14 +39,27 @@ const ImageClassifier = () => {
     return (
         <div id='categorizer'>
             <h1>Image Categorizer</h1>
+            {/* Buttons to scroll to categories */}
+            <div className="category-buttons">
+                {category.map(({ category_name }) => (
+                    <button
+                        className='goto-btns'
+                        key={category_name}
+                        onClick={() => scrollToCategory(category_name)}
+                        >
+                        {category_name}
+                    </button>
+                ))}
+            </div>
+            {/* Display categories */}
             {category && category.map(({ category_name, img_list }) => (
                 <div key={category_name} className='category'>
-                    <h1>{category_name}</h1>
+                    <h1 ref={(ele) => (categoryRefs.current[category_name] = ele)}>{category_name}</h1>
                     <div>
                         {img_list.map((img) => (
                             <img
-                                src={img}
-                                key={img}
+                            src={img}
+                            key={img}
                             />
                         ))}
                     </div>
