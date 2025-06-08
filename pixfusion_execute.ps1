e48f1f10f85f96288b6ed776a1e84c6b52cdd690
KarthikSapaliga <147088193+KarthikSapaliga@users.noreply.github.com>
2025-06-08 10:00:54 +0530

    Delete pixfusion_execute.ps1
    Co-Authored-By: Nithin Jayaprakash Nayak <nyknithin@gmail.com>
    Co-Authored-By: SwastikMoolya <156818163+SwastikMoolya@users.noreply.github.com>

--- pixfusion_execute.ps1
+++
@@ -1,3 +0,0 @@
-# Run the Python server and npm dev server simultaneously
-# Start-Process -NoNewWindow -FilePath "cmd" -ArgumentList "/c python server.py"
-# Start-Process -NoNewWindow -FilePath "cmd" -ArgumentList "/c npm run dev"
